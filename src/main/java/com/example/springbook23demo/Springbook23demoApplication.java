5f19aeec4eef7834dd0289ac1ee3fe9caa79802e
Osedumme Steven <stephen.osedumme@iths.se>
2023-04-02 00:55:44 +0200

    Initial commit

---
+++ src/main/java/com/example/springbook23demo/Springbook23demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbook23demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbook23demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbook23demoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbook23demo/Springbook23demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbook23demo;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Springbook23demoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
